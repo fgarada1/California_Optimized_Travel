b5ba9e7a7b0cc25dc35a621db0b05ebc954501ba
nithilak <90289415+nithilak@users.noreply.github.com>
2022-11-08 16:48:37 -0600

    Update Graph.h

--- src/Graph.h
+++ src/Graph.h
@@ -1,8 +1,22 @@
 #include <vector>
+#include <string>
+#include <map>
 
 class Graph {
     public:
+    Graph(const std::string& filename_nodes, const std::string& filename_edges);
+
 
     private:
-    
+    struct Node {
+        int id;
+        double latitude;
+        double longitude;
+    };
+    struct Connection {
+        Node node;
+        double distance;
+    };
+
+    std::map<Node, std::vector<Connection>> graph;
 };
