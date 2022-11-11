747f7b3022bd70d53fa2074effafb0d42fa6bf51
nithilak <90289415+nithilak@users.noreply.github.com>
2022-11-11 16:29:15 -0600

    Update main.cpp

--- entry/main.cpp
+++ entry/main.cpp
@@ -3,5 +3,8 @@
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
-    Graph graph("../example_nodes.txt", "../example_edges.txt");
+    // Graph graph("../example_nodes.txt", "../example_edges.txt", 41);
+    Graph graph("../example_nodes2.txt", "../example_edges2.txt", 11);
+    // graph.print_graph();
+    graph.print_predecessors();
 }
