0d02390fb01352b6c79ed086c9921d471a9ddc89
nithilak <90289415+nithilak@users.noreply.github.com>
2022-11-11 16:27:52 -0600

    Changed Graph to hold an adjacency matrix
    , and added extra helper functions

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -5,7 +5,7 @@
 #include <stdexcept>
 #include <sstream>
 
-Graph::Graph(const std::string& filename_nodes, const std::string& filename_edges) {
+Graph::Graph(const std::string& filename_nodes, const std::string& filename_edges, int total_nodes) : graph_(total_nodes, std::vector<Node*>(total_nodes, nullptr)), predecessor_(total_nodes, std::vector<int>(total_nodes, -1)){
     std::cout << "nodes" << std::endl;
     //read nodes
     std::ifstream ifs_nodes{filename_nodes};
@@ -19,11 +19,43 @@ Graph::Graph(const std::string& filename_nodes, const std::string& filename_edge
     std::string id;
     std::string latitude;
     std::string longitude;
-    while (ifs_nodes.good()) {
-        ifs_nodes >> id >> latitude >> longitude;
+    int count = 0;
+    while (ifs_nodes.good() && count < total_nodes) {
+        ifs_nodes >> id;
+        if (ifs_nodes.good()) {
+            ifs_nodes >> latitude;
+        } else {
+            throw std::invalid_argument("Format read error on line: " + std::to_string(count));
+        }
+        if (ifs_nodes.good()) {
+            ifs_nodes >> longitude;
+        } else {
+            throw std::invalid_argument("Format read error on line: " + std::to_string(count));
+        }
+
         std::cout << id << " " << latitude << " " << longitude << std::endl;
-        Node node{std::stoi(id), std::stod(latitude), std::stod(longitude)};
-        graph.insert(std::pair<Node, std::vector<Connection>>(node, {}));
+        int id2 = std::stoi(id);
+        Node node{id2, std::stod(latitude), std::stod(longitude)}; //this guarantees higher precision for the doubles
+        try {
+            graph_.at(id2).at(id2) = &node;
+            predecessor_.at(id2).at(id2) = id2;
+        } catch (std::exception& e) {
+            throw std::invalid_argument("total_nodes is fewer than the number of nodes provided: "
+                                                                    + std::to_string(total_nodes));
+        }
+        count++;
+    }
+    try {
+        ifs_nodes >> id;
+        if (!ifs_nodes.bad()) {
+            throw std::invalid_argument("total_nodes does not match the number of nodes provided: "
+                                    "document has extra entries on line: " + std::to_string(count));
+        }
+    } catch (std::exception& e) {
+    }
+    if (count != total_nodes) {
+        throw std::invalid_argument("total_nodes does not match the number of nodes provided: count: " 
+                             + std::to_string(count) + " total_nodes: " + std::to_string(total_nodes));
     }
 
 
@@ -42,4 +74,34 @@ Graph::Graph(const std::string& filename_nodes, const std::string& filename_edge
     while (std::getline(ifs_edges, input2)) {
     }
 
+}
+
+void Graph::print_graph() {
+    std::cout << '\n' << std::endl;
+    for (std::vector<Node*> vect : graph_) {
+        for (Node* node_ptr : vect) {
+    //         std::cout << node_ptr->id << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+void Graph::print_predecessors() {
+    std::cout << '\n' << std::endl;
+    for (std::vector<int> vect : predecessor_) {
+        for (int num : vect) {
+            std::cout << num << " ";
+        }
+        std::cout << '\n' << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void Graph::print(const std::vector<std::vector<Node*>>& graph) {
+    std::cout << '\n' << std::endl;
+    for (std::vector<Node*> vect : graph) {
+        for (Node* node_ptr : vect) {
+    //         std::cout << node_ptr->id << " ";
+        }
+    }
+    std::cout << std::endl;
 }
