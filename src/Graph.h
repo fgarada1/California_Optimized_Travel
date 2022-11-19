a60ebc5d90eccaa98c6680a52318169075a89cee
nithilak <90289415+nithilak@users.noreply.github.com>
2022-11-18 23:17:34 -0600

    Fixed Graph.h and Graph.cpp
    Made Graph.h and Graph.cpp much cleaner to work with

--- src/Graph.h
+++ src/Graph.h
@@ -4,11 +4,15 @@
 
 class Graph {
     public:
-    Graph(const std::string& filename_nodes, const std::string& filename_edges, int total_nodes);
+    //initalizes all member variables, does not apply any algorithms except for the heuristic for the A* algorithm
+    Graph(const std::string& filename_nodes, const std::string& filename_edges, unsigned total_nodes, unsigned total_edges);
 
+    //prints the adjacency matrix for graph_ (the adjacancy matrix representation of the road network)
     void print_graph();
+    //prints the contents of predecesor_, for use in floyd_warshall's algorithm
     void print_predecessors();
 
+    //deconstructor for all member variables in the graph, deletes new nodes (if there are any being used in the current graph)
     ~Graph();
 
 
@@ -19,13 +23,24 @@ class Graph {
         double longitude;
     };
     struct Connection {
-        Node node;
+        unsigned id_from;
+        unsigned id_to;
         double distance;
     };
 
     void print(const std::vector<std::vector<Node*>>& graph);
 
-    std::vector<std::vector<Node*>> graph_;
-    std::vector<std::vector<int>> predecessor_;
-};
+    //initial graphs
+    std::vector<std::vector<double>> graph_; //the adjacency matrix representtation of the graph network, each entry represents the one way distance from col to row
+    std::vector<std::vector<unsigned>> predecessor_; //the predecessor graph stored for floyd_warshall's algorithm, each entry represents the predecessor to go to from row to get to col in the end
 
+    //algorithms
+    std::vector<std::vector<double>> floyd_warshall_; //stems from a copy of graph_ upon initialization, stores one way shortest distance from col to row, based on all possible paths in the graph
+    std::vector<std::vector<double>> heuristic_; //(based on total geographical distance between nodes, direction does not matter here)
+
+    //vector (of nodes)
+    std::vector<Node> nodes_; //vector of nodes stored to calculate the heuristic storing adjacency matrix for the A* algorithm
+
+    unsigned total_nodes_;
+    unsigned total_edges_;
+};
