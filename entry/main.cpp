2da68c35deef26327e23761e003ec39b05fbc60b
nithilak <90289415+nithilak@users.noreply.github.com>
2022-11-18 23:43:23 -0600

    Changed example2 to be example3

--- entry/main.cpp
+++ entry/main.cpp
@@ -4,7 +4,8 @@
 int main() {
     std::cout << "Hello, world!" << std::endl;
     // Graph graph("../example_nodes.txt", "../example_edges.txt", 41);
-    Graph graph("../example_nodes2.txt", "../example_edges2.txt", 11);
-    // graph.print_graph();
+    // Graph graph("../example_nodes2.txt", "../example_edges2.txt", 11, 11); //supposed to cause an error on line 8 of "../example_edges2.txt"
+    Graph graph("../example_nodes3.txt", "../example_edges3.txt", 11, 11); //not supposed to cause an error
+    graph.print_graph();
     graph.print_predecessors();
 }
