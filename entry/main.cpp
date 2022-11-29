64fd58f84e3c4cf0bf90d73f44da24820c958687
nithilak <90289415+nithilak@users.noreply.github.com>
2022-11-29 16:56:37 -0600

    Update main.cpp
    moved example files into more_test folder

--- entry/main.cpp
+++ entry/main.cpp
@@ -3,9 +3,9 @@
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
-    // Graph graph("../example_nodes.txt", "../example_edges.txt", 41);
-    // Graph graph("../example_nodes2.txt", "../example_edges2.txt", 11, 11); //supposed to cause an error on line 8 of "../example_edges2.txt"
-    Graph graph("../example_nodes3.txt", "../example_edges3.txt", 11, 11); //not supposed to cause an error
+    // Graph graph("../more_test/example_nodes.txt", "../more_test/example_edges.txt", 41);
+    // Graph graph("../more_test/example_nodes2.txt", "../more_test/example_edges2.txt", 11, 11); //supposed to cause an error on line 8 of "../example_edges2.txt"
+    Graph graph("../more_test/example_nodes3.txt", "../more_test/example_edges3.txt", 11, 11); //not supposed to cause an error
     graph.print_graph();
     graph.print_predecessors();
 }
