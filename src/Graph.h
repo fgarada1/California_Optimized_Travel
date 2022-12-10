31e472723a8ddb08d50fe116424d46fc4dad3115
nithilak <90289415+nithilak@users.noreply.github.com>
2022-12-10 15:26:07 -0600

    Finished A* algorithm, no testing

--- src/Graph.h
+++ src/Graph.h
@@ -43,10 +43,13 @@ class Graph {
     std::vector<Node*> shortest_path_floyd_warshall(unsigned id_from, unsigned id_to);
     std::vector<Node*> shortest_path_floyd_warshall(Node* node_from, Node* node_to);
 
-    //returns the shortest path as a std::vector<Node*> from node_from to node_to, based on the dijkstra's algorithm, if no such path exists, returns an empty vector
+    //returns the shortest path as a std::vector<Node*> from node_from to node_to, based on the dijkstra's algorithm, if no such path exists, returns a vector with only the starting node
     std::vector<Node*> compute_dijkstra_path(unsigned id_from, unsigned id_to);
     std::vector<Node*> compute_dijkstra_path(Node* node_from, Node* node_to);
 
+    //returns the shortest path as a std::vector<Node*> from node_from to node_to, based on the A* algorithm, if no such path exists, returns a vector with only the starting node
+    std::vector<Node*> compute_astar_path(unsigned id_from, unsigned id_to);
+    std::vector<Node*> compute_astar_path(Node* node_from, Node* node_to);
 
     //deconstructor for all member variables in the graph, deletes new nodes (if there are any being used in the current graph)
     ~Graph();
